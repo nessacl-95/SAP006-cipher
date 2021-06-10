fb3266b4d189f028f485e14fe84fd1591e2b5fb3
nessacl-95 <vanessalima951@gmail.com>
2021-06-10 13:51:15 -0300

    html semi-pronto, css básico e js finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,35 @@
-const cipher = {
-  // ...
-};
+let decifrar = [];
+let cifrar = [];
 
-export default cipher;
+function cipherEncode(string, offset){
+  let newMessage = "";
+  for(let i=0; i <= string.length; i++){
+    let codeString = string.charCodeAt(i);
+    let startCapitalLetters = 65;
+    let startSmallLetters = 97;
+    if((codeString >= 65) && (codeString <= 90)){
+      cifrar.push(String.fromCharCode(((codeString - startCapitalLetters + offset)% 26)+ startCapitalLetters));
+    } else if((codeString >= 97) && (codeString <= 122)){
+      cifrar.push(String.fromCharCode(((codeString - startSmallLetters + offset)% 26)+ startSmallLetters));
+    }
+  }
+  newMessage = (cifrar.join(""));
+  return newMessage
+}
+
+function cipherDecode(string, offset){
+  let newMessage = ""; 
+  for(let i=0; i <= string.length; i++){
+    let codeString = string.charCodeAt(i);
+    let startCapitalLetters = 65;
+    let startSmallLetters = 97;
+    if((codeString >= 65) && (codeString <= 90)){
+      decifrar.push(String.fromCharCode(((codeString - startCapitalLetters - offset)% 26)+ startCapitalLetters));
+    } else if((codeString >= 97) && (codeString <= 122)){
+      decifrar.push(String.fromCharCode(((codeString - startSmallLetters - offset)% 26)+ startSmallLetters));
+    }
+  }
+  newMessage = (decifrar.join(""));
+  return newMessage
+}
+export default cipherEncode; cipherDecode;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import cipher from './cipher.js';
+import cipherEncode from './cipher.js';
+import cipherDecode from './cipher.js';
 
-console.log(cipher);
+document.getElementById("cifrarMensagem").addEventListener("click", encode);
+document.getElementById("decifrarMensagem").addEventListener("click", decode);
+
+function encode(){
+    let string = document.getElementById("cifrarString").value
+    let offset = parseInt(document.getElementById("cifrarOffset").value)
+    let result = cipherEncode(string, offset);
+
+    document.getElementById("resultCifrar").innerHTML = result;
+}
+
+function decode(){
+    let string = document.getElementById("decifrarString").value
+    let offset = -(parseInt(document.getElementById("decifrarOffset")).value)
+    let result = cipherDecode (string, offset);
+
+    document.getElementById("resultDecifrar").innerHTML = result;
+}
