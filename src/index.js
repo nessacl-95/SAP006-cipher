bafa8433055033c280bd98f105495e2b34b28afc
nessacl-95 <vanessalima951@gmail.com>
2021-06-15 12:40:58 -0300

    Alterações no HTML e CSS, Readme finalizado

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import cipher from './cipher.js';
 document.getElementById("cifrarMensagem").addEventListener("click", cipherEncode);
 document.getElementById("decifrarMensagem").addEventListener("click", cipherDecode);
 
+
+
 function cipherEncode(e){
     let string = document.getElementById("inputCifra").value
     let offset = parseInt(document.getElementById("cifrarOffset").value)
